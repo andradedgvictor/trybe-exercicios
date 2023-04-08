1f18876724d2250209c884f1894f2b232204af10
andradedgvictor <victorandrademg@outlook.com>
2023-04-08 16:48:52 -0300

    add exercise 6

---
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercicio6.js
@@ -0,0 +1,14 @@
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { spring, summer, autumn, winter } = yearSeasons;
+
+const monthsOfTheYear = [...winter, ...spring, ...summer, ...autumn];
+
+console.log(monthsOfTheYear)
+  
