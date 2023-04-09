dd198a8a0a7fa56e0cbd8af86361ed15462c451f
andradedgvictor <victorandrademg@outlook.com>
2023-04-09 17:32:04 -0300

    add bonus

--- fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,12 +1,20 @@
 const clearBtn = document.querySelector('#clear-btn');
 const submitBtn = document.querySelector('#submit-btn');
 const agreement = document.querySelector('#agreement');
-const form = document.querySelector('#main-form');
+const form = document.querySelector('.form-trip');
+
+const clearForm = () => {
+  form.reset();
+};
+
+const enableSubmit = () => {
+  submitBtn.disabled = !agreement.checked;
+};
 
 const validateForm = (event) => {
   const fullNameLength = fullName.value.length;
   const emailLength = email.value.length;
-  const questionLength = question.value.length;
+  const whyLength = why.value.length;
 
   if (fullNameLength > 40) {
     alert('O nome completo deve ter no máximo 40 caracteres.');
@@ -18,20 +26,11 @@ const validateForm = (event) => {
     event.preventDefault();
   }
 
-  if (questionLength > 500) {
-    alert('O campo de texto deve ter no máximo 500 caracteres.');
+  if (whyLength > 500) {
+    alert('O campo "Por que você deveria ser a pessoa desenvolvedora escolhida pelo concurso TrybeTrip?" deve ter no máximo 500 caracteres.');
     event.preventDefault();
   }
 };
 
-const clearForm = () => {
-  form.reset();
-};
-
-const enableSubmit = () => {
-  submitBtn.disabled = !agreement.checked;
-};
-
-agreement.addEventListener('change', enableSubmit);
-form.addEventListener('submit', validateForm);
 clearBtn.addEventListener('click', clearForm);
+agreement.addEventListener('change', enableSubmit);
