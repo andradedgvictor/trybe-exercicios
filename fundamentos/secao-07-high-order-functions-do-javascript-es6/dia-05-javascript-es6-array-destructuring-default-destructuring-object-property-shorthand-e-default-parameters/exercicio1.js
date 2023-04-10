310859c770eb49098f9fc2bd20b327b4fe3151e1
andradedgvictor <victorandrademg@outlook.com>
2023-04-10 15:30:48 -0300

    add files fundamentos7.5

---
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercicio1.js
@@ -0,0 +1,3 @@
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
---
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercicio5.js
@@ -0,0 +1,10 @@
+const moreStudents = [
+    'Chris', 
+    ['Ahmad', 'Antigoni'], 
+    ['Toby', 'Sam']
+  ];
+
+  // Escreva seu código aqui
+  const [] = moreStudents;
+
+console.log(student1, student2, student3, student4, student5);
