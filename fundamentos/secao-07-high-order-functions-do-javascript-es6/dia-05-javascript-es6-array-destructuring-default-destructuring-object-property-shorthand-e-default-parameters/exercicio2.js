ab4f503aebadcf89313bd3a2927f91495daf205f
andradedgvictor <victorandrademg@outlook.com>
2023-04-10 15:52:40 -0300

    add exercise 2 fundamentos 7.5

--- fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercicio2.js
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercicio2.js
@@ -2,4 +2,4 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva toObject abaixo
+const toObject = ([car, brand, year]) => ({ car, brand, year });
