a4ffa9026984c87fba160af514cf589e65d8bfb1
andradedgvictor <victorandrademg@outlook.com>
2023-05-03 13:15:33 -0300

    add alert

--- 02-Front-end/dia-02-javaScript-assíncrono-promises-e-fetch/superheroapi/src/main.js
+++ 02-Front-end/dia-02-javaScript-assíncrono-promises-e-fetch/superheroapi/src/main.js
@@ -1,4 +1,7 @@
+import Swal from 'sweetalert2';
+
 /* eslint-disable no-magic-numbers */
+
 const containerInfo = document.getElementsByClassName('superheroe-content')[0];
 const button = document.getElementById('sortBtn');
 const img = document.createElement('img');
@@ -6,13 +9,22 @@ const pName = document.getElementsByTagName('p')[0];
 
 button.addEventListener('click', async (event) => {
     event.preventDefault();
-    const myToken = 3119348995037520;
-    const characterApi = `https://superheroapi.com/api.php/${myToken}`;
-    const characterId = Math.floor(Math.random() * 732) + 1;
-    const characterInfoApi = `${characterApi}/${characterId}/image`;
-    const response = await fetch(characterInfoApi);
-    const data = await response.json();
-    img.src = data.url;
-    containerInfo.insertAdjacentElement('afterbegin', img);
-    pName.innerHTML = data.name;
+    try {
+        const myToken = 3119348995037520;
+        const characterApi = `https://superheroapi.com/api.php/${myToken}`;
+        const characterId = Math.floor(Math.random() * 732) + 1;
+        const characterInfoApi = `${characterApi}/${characterId}/image`;
+        const response = await fetch(characterInfoApi);
+        const data = await response.json();
+        img.src = data.url;
+        containerInfo.insertAdjacentElement('afterbegin', img);
+        pName.innerHTML = data.name;
+    } catch (error) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Tente novamente!',
+            icon: 'error',
+            confirmButtonText: 'OK!',
+        });
+    }
 });
